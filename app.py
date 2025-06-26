b33d87dee16585c9783763bac6804d0f1411aebe
poppyts25 <126086359+poppyts25@users.noreply.github.com>
2025-06-26 13:44:29 +0100

    error catching

--- app.py
+++ app.py
@@ -126,11 +126,19 @@ def get_area(isochrone_polygon, centre):
 
     # **Reproject to UTM for accurate area calculation**
     isochrone_gdf = isochrone_gdf.to_crs(epsg=epsg_code)
-    # **Calculate the area in square meters**
+    # **Calculate the area in square metres**
     area = isochrone_gdf.geometry.area.iloc[0]/1000000
-    print("Area in square meters:", area)
+    print("Area in square metres:", area)
     area = round(area, 2)
     return area
+
+def add_error_alert(m, error_message):
+    # Add an alert to the map
+    m.get_root().html.add_child(folium.Element(f"""
+        <script>
+        alert("{error_message}");
+        </script>
+    """))
 
 @app.route('/')
 def index():
@@ -164,7 +172,8 @@ def index():
 
 # Main route for the index page
 @app.route("/", methods=["GET", "POST"])
-def main_index():
+@app.route("/home", methods=["GET", "POST"])
+def main_page():
     map_path = "static/map.html"
 
     if request.method == "POST":
@@ -172,11 +181,21 @@ def main_index():
         travel_method = request.form.get("travel_method")
         speed = request.form.get("speed")
         amenity = request.form.get("amenity")
+
+        # If latitude or longitude is not provided set to exeter city centre
+        try:
+            centre_lat = float(request.form.get("latitude"))
+            centre_lon = float(request.form.get("longitude"))
+        except (ValueError, TypeError):
+            centre_lat = 50.7260
+            centre_lon = -3.5275  # Example: exeter, UK
         
-        center_lat = float(request.form.get("latitude"))
-        center_lon = float(request.form.get("longitude"))
-        center_point = (center_lat, center_lon)
+        centre_point = (centre_lat, centre_lon)
         
+        # Create a Folium map
+        m = folium.Map(location=centre_point, zoom_start=13, max_bounds=True, min_zoom=2)  
+
+
         # Define the speed based on user input
         travel_time = 15  # in minutes
         if travel_method == "Walk":
@@ -188,53 +207,78 @@ def main_index():
             distance =  5000 if speed == "S" else 7000 if speed == "A" else 9000
             travel = "bike"
             
-        speed_mps = travel_speed * 1000 / 3600  # Convert to meters per second
+        speed_mps = travel_speed * 1000 / 3600  # Convert to metres per second
         travel_time_seconds = 15 * 60  # 15 minutes
         travel_radius = travel_time_seconds * speed_mps
 
         ## Load network graph
         ox.settings.use_cache = True  # Enable caching to avoid repeated downloads
-        G = ox.graph_from_point((center_lat, center_lon), network_type=travel, dist=distance, simplify=True, dist_type="network")
-         
-        center_node = ox.distance.nearest_nodes(G, center_lon, center_lat)
+        try:
+            G = ox.graph_from_point((centre_lat, centre_lon), network_type=travel, dist=distance, simplify=True, dist_type="network")
+        except Exception as e:
+            print(f"Error loading graph: {e}")           
 
-        isochrone_subgraph = nx.ego_graph(G, center_node, radius=travel_radius, distance="length")
-        nodes, edges = ox.graph_to_gdfs(isochrone_subgraph, nodes=True, edges=True)
-        # Extract node coordinates from the isochrone subgraph
-        node_points = np.array(list(zip(nodes.geometry.x, nodes.geometry.y)))
+            error_message = "Error loading the network graph. There are no roads in this area or the area is too small. Please try a different location."
+            add_error_alert(m, error_message)
+            save_map(m, map_path)
+            G = None    
 
-        # Adjust alpha parameter (lower values give more detail)
-        alpha = 0.3  # Try tweaking between 0.1 and 1.0 for best results
-        isochrone_polygon = alphashape(node_points, alpha)
+            form_data = {
+                "travel_method": request.form.get("travel_method", ""),
+                "speed": request.form.get("speed", ""),
+                "amenity": request.form.get("amenity", ""),
+                "latitude": None,
+                "longitude": None
+            }
 
-        # Create a Folium map
-        m = folium.Map(location=center_point, zoom_start=13, max_bounds=True, min_zoom=2)  
+            stats = {
+                "markers": None,
+                "area": None
+                }
 
-        # Add the isochrone polygon to the map
-        style_func = lambda x: {"fillColor": "red", "color": "red", "weight": 0, "fillOpacity": 0.4}
-        if isinstance(isochrone_polygon, MultiPolygon):
-            for poly in isochrone_polygon.geoms:
-                folium.GeoJson(poly, style_function=style_func).add_to(m)
-        else:
-            folium.GeoJson(isochrone_polygon, style_function=style_func).add_to(m)
+            return render_template('index.html', map_url=map_path, form_data=form_data, stats=stats)
+            
+
+        if G is not None:
+            centre_node = ox.distance.nearest_nodes(G, centre_lon, centre_lat)
+
+            isochrone_subgraph = nx.ego_graph(G, centre_node, radius=travel_radius, distance="length")
+            nodes, edges = ox.graph_to_gdfs(isochrone_subgraph, nodes=True, edges=True)
+            # Extract node coordinates from the isochrone subgraph
+            node_points = np.array(list(zip(nodes.geometry.x, nodes.geometry.y)))
+
+            # Adjust alpha parameter (lower values give more detail)
+            alpha = 0.3  # Try tweaking between 0.1 and 1.0 for best results
+            isochrone_polygon = alphashape(node_points, alpha)
+
+          
+            # Add the isochrone polygon to the map
+            style_func = lambda x: {"fillColor": "red", "color": "red", "weight": 0, "fillOpacity": 0.4}
+            if isinstance(isochrone_polygon, MultiPolygon):
+                for poly in isochrone_polygon.geoms:
+                    folium.GeoJson(poly, style_function=style_func).add_to(m)
+            else:
+                folium.GeoJson(isochrone_polygon, style_function=style_func).add_to(m)
 
 
-        markers=0
-        if amenity != "none":
-            m = add_amenities(m, center_point, isochrone_polygon, amenity, distance)
-            markers = count_markers(m)
+            markers=0
+            if amenity != "none":
+                m = add_amenities(m, centre_point, isochrone_polygon, amenity, distance)
+                markers = count_markers(m)
 
-        # Add marker for centre point, selected by user or default centre
-        folium.Marker(
-            location=[center_lat, center_lon],
-            popup="Centre Point",
-            icon=folium.Icon(color="lightgray")
-        ).add_to(m)
+            # Add marker for centre point, selected by user or default centre
+            folium.Marker(
+                location=[centre_lat, centre_lon],
+                popup="Centre Point",
+                icon=folium.Icon(color="lightgray")
+            ).add_to(m)
 
 
-          # Serve the map from the static directory
-        # save the map with the click event
-        save_map(m, map_path)               
+            # Serve the map from the static directory
+            # save the map with the click event
+            save_map(m, map_path)
+
+    
 
     form_data = {
         "travel_method": request.form.get("travel_method", ""),
@@ -244,7 +288,7 @@ def main_index():
         "longitude": request.form.get("longitude", "")
     }
 
-    area = get_area(isochrone_polygon, center_point)
+    area = get_area(isochrone_polygon, centre_point)
 
     stats = {
         "markers": markers, 
@@ -295,19 +339,31 @@ def compare_index():
 # compare areas page
 @app.route("/compare", methods=["GET", "POST"])
 def compare_areas():
+    map_path_1 = "static/area1.html"  # Serve the map from the static directory
+    map_path_2 = "static/area2.html"
     if request.method == "POST":
         # Retrieve user input from the form
         travel_method = request.form.get("travel_method")
         speed = request.form.get("speed")
         amenity = request.form.get("amenity")
         
-        center_lat_1 = float(request.form.get("latitude1"))
-        center_lon_1 = float(request.form.get("longitude1"))
-        center_point_1 = (center_lat_1, center_lon_1)
+        try:
+            centre_lat_1 = float(request.form.get("latitude1"))
+            centre_lon_1 = float(request.form.get("longitude1"))
+        except (ValueError, TypeError):
+            centre_lat_1 = 50.7260
+            centre_lon_1 = -3.5275
 
-        center_lat_2 = float(request.form.get("latitude2"))
-        center_lon_2 = float(request.form.get("longitude2"))
-        center_point_2 = (center_lat_2, center_lon_2)
+        centre_point_1 = (centre_lat_1, centre_lon_1)
+
+        try:
+            centre_lat_2 = float(request.form.get("latitude2"))
+            centre_lon_2 = float(request.form.get("longitude2"))
+        except (ValueError, TypeError):
+            centre_lat_2 = 50.7260
+            centre_lon_2 = -3.5275
+
+        centre_point_2 = (centre_lat_2, centre_lon_2)
 
         # Define the speed based on user input
         travel_time = 15  # in minutes
@@ -319,83 +375,143 @@ def compare_areas():
             travel_speed = 14 if speed == "S" else 16 if speed == "A" else 18
             distance =  5000 if speed == "S" else 7000 if speed == "A" else 9000
             travel = "bike"
-        speed_mps = travel_speed * 1000 / 3600  # Convert to meters per second
+        speed_mps = travel_speed * 1000 / 3600  # Convert to metres per second
         travel_time_seconds = 15 * 60  # 15 minutes
         travel_radius = travel_time_seconds * speed_mps
 
 
         ## Load network graph
         ox.settings.use_cache = True  # Enable caching to avoid repeated downloads
-        G1 = ox.graph_from_point((center_lat_1, center_lon_1), network_type=travel, dist=distance, simplify=True, dist_type="network")
-        G2 = ox.graph_from_point((center_lat_2, center_lon_2), network_type=travel, dist=distance, simplify=True, dist_type="network")
-
-
-        center_node_1 = ox.distance.nearest_nodes(G1, center_lon_1, center_lat_1)
-        center_node_2 = ox.distance.nearest_nodes(G2, center_lon_2, center_lat_2)
-
-        isochrone_subgraph_1 = nx.ego_graph(G1, center_node_1, radius=travel_radius, distance="length")
-        isochrone_subgraph_2 = nx.ego_graph(G2, center_node_2, radius=travel_radius, distance="length")
-
-        nodes_1, edges_1 = ox.graph_to_gdfs(isochrone_subgraph_1, nodes=True, edges=True)
-        nodes_2, edges_2 = ox.graph_to_gdfs(isochrone_subgraph_2, nodes=True, edges=True)
-        # Extract node coordinates from the isochrone subgraph
-        node_points_1 = np.array(list(zip(nodes_1.geometry.x, nodes_1.geometry.y)))
-        node_points_2 = np.array(list(zip(nodes_2.geometry.x, nodes_2.geometry.y)))
-
-        # Adjust alpha parameter (lower values give more detail)
-        alpha = 0.3 
-        isochrone_polygon_1 = alphashape(node_points_1, alpha)
-        isochrone_polygon_2 = alphashape(node_points_2, alpha)
-
-        # Create a Folium map
-        m1 = folium.Map(location=center_point_1, zoom_start=13,max_bounds=True, min_zoom=2)
-        m2 = folium.Map(location=center_point_2, zoom_start=13, max_bounds=True, min_zoom=2 )
-
-        # Add the isochrone polygon to the map
-        style_func = lambda x: {"fillColor": "red", "color": "red", "weight": 0, "fillOpacity": 0.4}
-        if isinstance(isochrone_polygon_1, MultiPolygon):
-            for poly in isochrone_polygon_1.geoms:
-                folium.GeoJson(poly, style_function=style_func).add_to(m1)
-        else:
-            folium.GeoJson(isochrone_polygon_1, style_function=style_func).add_to(m1)
-
-        if isinstance(isochrone_polygon_2, MultiPolygon):
-            for poly in isochrone_polygon_2.geoms:
-                folium.GeoJson(poly, style_function=style_func).add_to(m2)
-        else:
-            folium.GeoJson(isochrone_polygon_2, style_function=style_func).add_to(m2)
-
-
-        markers_1=0
-        markers_2=0
-        # (Optional) Add amenity markers if applicable
-        if amenity != "none":
-            m1 = add_amenities(m1, center_point_1, isochrone_polygon_1, amenity, distance)
-            markers_1 = count_markers(m1)
-
-            m2 = add_amenities(m2, center_point_2, isochrone_polygon_2, amenity, distance)
-            markers_2 = count_markers(m2)
-
-
-        # Add marker for centre point, selected by user or default centre
-        folium.Marker(
-            location=[center_lat_1, center_lon_1],
-            popup="Centre Point",
-            icon=folium.Icon(color="lightgray")
-        ).add_to(m1)
-
-
-        folium.Marker(
-            location=[center_lat_2, center_lon_2],
-            popup="Centre Point",
-            icon=folium.Icon(color="lightgray")
-        ).add_to(m2)
-
         
-        map_path_1 = "static/area1.html"  # Serve the map from the static directory
-        map_path_2 = "static/area2.html"
-        save_map(m1, map_path_1)
-        save_map(m2, map_path_2)
+        try:
+            G1 = ox.graph_from_point((centre_lat_1, centre_lon_1), network_type=travel, dist=distance, simplify=True, dist_type="network")
+        except Exception as e:
+            print(f"Error loading graph for area 1: {e}")           
+
+            error_message = "Error loading the network graph for area 1. There are no roads in this area or the area is too small. Please try a different location."
+            m1 = folium.Map(location=centre_point_1, zoom_start=13, max_bounds=True, min_zoom=2)
+            add_error_alert(m1, error_message)
+            save_map(m1, map_path_1)
+            G1 = None
+
+            form_data = {
+                "travel_method": request.form.get("travel_method", ""),
+                "speed": request.form.get("speed", ""),
+                "amenity": request.form.get("amenity", ""),
+                "latitude1": request.form.get("latitude1", ""),
+                "longitude1": request.form.get("longitude1", ""),
+                "latitude2": request.form.get("latitude2", ""),
+                "longitude2": request.form.get("longitude2", "")
+            }
+
+
+            stats = {
+                "markers_1": None,
+                "markers_2": None,
+                "area_1": None,
+                "area_2": None
+            }
+
+            # Render the compare.html page with the map URL
+            return render_template('compare_area.html', map_url_1=map_path_1, map_url_2=map_path_2, form_data=form_data, stats=stats)
+            
+        try:
+            G2 = ox.graph_from_point((centre_lat_2, centre_lon_2), network_type=travel, dist=distance, simplify=True, dist_type="network")
+        except Exception as e:
+            print(f"Error loading graph for area 2: {e}")           
+
+            error_message = "Error loading the network graph for area 2. There are no roads in this area or the area is too small. Please try a different location."
+            m2 = folium.Map(location=centre_point_2, zoom_start=13, max_bounds=True, min_zoom=2)
+            add_error_alert(m2, error_message)
+            save_map(m2, map_path_2)
+            G2 = None
+
+
+            form_data = {
+                "travel_method": request.form.get("travel_method", ""),
+                "speed": request.form.get("speed", ""),
+                "amenity": request.form.get("amenity", ""),
+                "latitude1": request.form.get("latitude1", ""),
+                "longitude1": request.form.get("longitude1", ""),
+                "latitude2": request.form.get("latitude2", ""),
+                "longitude2": request.form.get("longitude2", "")
+            }
+
+            stats = {
+                "markers_1": None,
+                "markers_2": None,
+                "area_1": None,
+                "area_2": None
+                }
+
+            # Render the compare.html page with the map URL
+            return render_template('compare_area.html', map_url_1=map_path_1, map_url_2=map_path_2, form_data=form_data, stats=stats)
+
+        if (G1 is not None) and (G2 is not None):
+            centre_node_1 = ox.distance.nearest_nodes(G1, centre_lon_1, centre_lat_1)
+            centre_node_2 = ox.distance.nearest_nodes(G2, centre_lon_2, centre_lat_2)
+
+            isochrone_subgraph_1 = nx.ego_graph(G1, centre_node_1, radius=travel_radius, distance="length")
+            isochrone_subgraph_2 = nx.ego_graph(G2, centre_node_2, radius=travel_radius, distance="length")
+
+            nodes_1, edges_1 = ox.graph_to_gdfs(isochrone_subgraph_1, nodes=True, edges=True)
+            nodes_2, edges_2 = ox.graph_to_gdfs(isochrone_subgraph_2, nodes=True, edges=True)
+            # Extract node coordinates from the isochrone subgraph
+            node_points_1 = np.array(list(zip(nodes_1.geometry.x, nodes_1.geometry.y)))
+            node_points_2 = np.array(list(zip(nodes_2.geometry.x, nodes_2.geometry.y)))
+
+            # Adjust alpha parameter (lower values give more detail)
+            alpha = 0.3 
+            isochrone_polygon_1 = alphashape(node_points_1, alpha)
+            isochrone_polygon_2 = alphashape(node_points_2, alpha)
+
+            # Create a Folium map
+            m1 = folium.Map(location=centre_point_1, zoom_start=13,max_bounds=True, min_zoom=2)
+            m2 = folium.Map(location=centre_point_2, zoom_start=13, max_bounds=True, min_zoom=2 )
+
+            # Add the isochrone polygon to the map
+            style_func = lambda x: {"fillColor": "red", "color": "red", "weight": 0, "fillOpacity": 0.4}
+            if isinstance(isochrone_polygon_1, MultiPolygon):
+                for poly in isochrone_polygon_1.geoms:
+                    folium.GeoJson(poly, style_function=style_func).add_to(m1)
+            else:
+                folium.GeoJson(isochrone_polygon_1, style_function=style_func).add_to(m1)
+
+            if isinstance(isochrone_polygon_2, MultiPolygon):
+                for poly in isochrone_polygon_2.geoms:
+                    folium.GeoJson(poly, style_function=style_func).add_to(m2)
+            else:
+                folium.GeoJson(isochrone_polygon_2, style_function=style_func).add_to(m2)
+
+
+            markers_1=0
+            markers_2=0
+            # (Optional) Add amenity markers if applicable
+            if amenity != "none":
+                m1 = add_amenities(m1, centre_point_1, isochrone_polygon_1, amenity, distance)
+                markers_1 = count_markers(m1)
+
+                m2 = add_amenities(m2, centre_point_2, isochrone_polygon_2, amenity, distance)
+                markers_2 = count_markers(m2)
+
+
+            # Add marker for centre point, selected by user or default centre
+            folium.Marker(
+                location=[centre_lat_1, centre_lon_1],
+                popup="Centre Point",
+                icon=folium.Icon(color="lightgray")
+            ).add_to(m1)
+
+
+            folium.Marker(
+                location=[centre_lat_2, centre_lon_2],
+                popup="Centre Point",
+                icon=folium.Icon(color="lightgray")
+            ).add_to(m2)
+
+            
+            save_map(m1, map_path_1)
+            save_map(m2, map_path_2)
 
 
     form_data = {
@@ -408,8 +524,8 @@ def compare_areas():
         "longitude2": request.form.get("longitude2", "")
     }
 
-    area_1 = get_area(isochrone_polygon_1, center_point_1)
-    area_2 = get_area(isochrone_polygon_2, center_point_2)
+    area_1 = get_area(isochrone_polygon_1, centre_point_1)
+    area_2 = get_area(isochrone_polygon_2, centre_point_2)
 
     stats = {
         "markers_1": markers_1,
